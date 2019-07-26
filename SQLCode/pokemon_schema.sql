cde9aaafc6925a2671fe88f13a5e8d17d1b3d735
Raj Shah <rshah7255@gmail.com>
2019-07-25 20:49:30 -0400

    Changes by Raj 7.25.19

--- SQLCode/pokemon_schema.sql
+++ SQLCode/pokemon_schema.sql
@@ -1,26 +1,23 @@
-﻿
+-- DROP TABLE "Types";
+CREATE TABLE "Types" (
+    "id" INT NOT NULL PRIMARY KEY,
+	"TID" INT  NOT NULL UNIQUE,
+    "Type" VARCHAR(255)   NOT NULL
+);
+
 -- DROP TABLE "Pokemon";
 CREATE TABLE "Pokemon" (
-    "Pokemon_Name" VARCHAR(255)   NOT NULL PRIMARY KEY,
-    "TID" INT   NOT NULL,
+    "Pokemon_Name" VARCHAR(255)  NOT NULL PRIMARY KEY,
+    "TID" INT NOT NULL REFERENCES "Types"("TID"),
+	"Type" VARCHAR(255)   NOT NULL,
     "Attack" INT   NOT NULL,
     "Defense" INT   NOT NULL,
     "Speed" INT   NOT NULL
 );
 
-CREATE TABLE "Types" (
-    "TID" INT   NOT NULL PRIMARY KEY,
-    "Type" VARCHAR(255)   NOT NULL
-);
-
+-- DROP TABLE "Moves";
 CREATE TABLE "Moves" (
     "MID" INT   NOT NULL PRIMARY KEY,
     "Moves" VARCHAR(255)   NOT NULL,
-    "TID" INT   NOT NULL
+    "TID" INT   NOT NULL REFERENCES "Types"("TID")
 );
-
-ALTER TABLE "Pokemon" ADD CONSTRAINT fk_Pokemon_Type FOREIGN KEY("TID")
-REFERENCES "Types" ("TID");
-
-ALTER TABLE "Moves" ADD CONSTRAINT "fk_Moves_TID" FOREIGN KEY("TID")
-REFERENCES "Types" ("TID");
